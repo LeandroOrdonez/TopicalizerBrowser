c219bc36c7962adab968717828fa4d44c2b4b814
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2014-03-02 21:15:56 -0500

    Added index.xhtml, login.css, login.js, and login-radius and bootstrap-social libraries for building the login page.

---
+++ web/js/login.js
@@ -0,0 +1,32 @@
+var options = {};
+options.login = true;
+LoginRadius_SocialLogin.util.ready(function() {
+    $ui = LoginRadius_SocialLogin.lr_login_settings;
+    $ui.interfacesize = "";
+    $ui.apikey = "fbbb5623-ab4c-4fc2-9d18-dee0204b151c";
+    $ui.callback = "";
+    $ui.lrinterfacecontainer = "interfacecontainerdiv";
+    LoginRadius_SocialLogin.init(options);
+});
+
+
+LoginRadiusSDK.onlogin = Successfullylogin;
+function Successfullylogin() {
+    console.log("Successfullylogin...");
+    LoginRadiusSDK.getUserprofile(function(data) {
+        console.log(data);
+        console.log("Data provider: " + data.Provider);
+        console.log("Data First Name: " + data.FirstName);
+        console.log("Data ID: " + data.ID);
+        alert("Welcome " + data.FirstName + "!");
+
+//        document.getElementById('user-profile').value = JSON.stringify(data);
+//        document.getElementById('provider').value = (data.Provider);
+//        document.getElementById('buttonId').click();
+//
+//        document.getElementById("userID").innerHTML = data.ID;
+//        $("#userID").slideDown("slow");
+    });
+
+    return false;
+};
